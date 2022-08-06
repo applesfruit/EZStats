ba04511ec560bb225751c9d45b9ef190fd6c4e6b
applesfruit <83845564+applesfruit@users.noreply.github.com>
2022-08-05 21:15:38 -0700

    cleanup + not properly updated src
    0.7 is current src

--- src/main/java/me/applesfruit/ezstats/gui/ComponentGUI.java
+++
@@ -1,9 +0,0 @@
-package me.applesfruit.ezstats.gui;
-
-import net.minecraft.client.gui.GuiScreen;
-
-public class ComponentGUI extends GuiScreen {
-
-
-
-}
---
+++ src/main/java/me/applesfruit/ezstats/gui/components/impl/Latency.java
@@ -0,0 +1,25 @@
+package me.applesfruit.ezstats.gui.components.impl;
+
+import me.applesfruit.ezstats.gui.components.gui.DragGUI;
+
+public class Latency extends DragGUI {
+
+
+    @Override
+    public int getWidth() {
+        return 0;
+    }
+
+    @Override
+    public int getHeight() {
+        return 0;
+    }
+
+    @Override
+    public void drawUI() {
+        if (this.scale != 0.0f)
+        {
+            
+        }
+    }
+}
