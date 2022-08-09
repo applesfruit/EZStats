193c7547fe279493223cb58e672836edd8bf8ae7
poiu <75510171+notpoiu@users.noreply.github.com>
2022-08-09 10:49:31 -0400

    fruits is fruits is fruits is fruits

--- src/main/java/me/applesfruit/ezstats/gui/components/impl/Latency.java
+++ src/main/java/me/applesfruit/ezstats/gui/components/impl/Latency.java
@@ -1,25 +1,65 @@
 package me.applesfruit.ezstats.gui.components.impl;
 
+import me.applesfruit.ezstats.EZStats;
 import me.applesfruit.ezstats.gui.components.gui.DragGUI;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.client.gui.GuiPlayerInfo;
+import net.minecraft.client.network.NetHandlerPlayClient;
+import net.minecraft.entity.player.EntityPlayerMP;
+import org.lwjgl.opengl.GL11;
+
+import java.util.List;
 
 public class Latency extends DragGUI {
 
+    private int latency = 0;
+    public boolean enabled = true;
+    public Latency(EZStats instance, int x, int y, float scale) {
+        super(instance, x, y, scale);
+    }
 
     @Override
     public int getWidth() {
-        return 0;
+        return mc.fontRenderer.getStringWidth("Ping: " + latency) + 4;
     }
 
     @Override
     public int getHeight() {
-        return 0;
+        return mc.fontRenderer.FONT_HEIGHT;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
     }
 
     @Override
     public void drawUI() {
         if (this.scale != 0.0f)
         {
+            FontRenderer fr =  this.mc.fontRenderer;
+            NetHandlerPlayClient handler = mc.thePlayer.sendQueue;
+            List<GuiPlayerInfo> playerInfoList = handler.playerInfoList;
+            playerInfoList.stream()
+                    .filter(EntityPlayerSP.class::isInstance);
+
+            GuiPlayerInfo player = (GuiPlayerInfo) handler.playerInfoList.get(0); // idk but it works lol
             
+            if(mc.isSingleplayer())
+                latency = 0;
+            else
+                latency = player.responseTime;
+
+            int mx = this.posX + (this.getWidth() - fr.getStringWidth("Ping: " + latency)) / 2;
+            int my = this.posY + (this.getHeight() - fr.FONT_HEIGHT) / 2 + 1;
+
+            GL11.glEnable(3042);
+            fr.drawString("Ping: " + latency, mx, my, this.instance.cpsColor.getValue().intValue());
+            GL11.glDisable(3042);
         }
     }
 }
